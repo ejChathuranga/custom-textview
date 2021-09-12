4857d8b01318cf52e4e5d68303693cd92006b203
EJ <ejcindunil@gmail.com>
2021-09-12 09:15:27 +0530

    add setUnderlineDotWidthAndSpace, setUnderlineThickness

--- app/src/main/java/io/github/ejchathuranga/customtextview/MainActivity.java
+++ app/src/main/java/io/github/ejchathuranga/customtextview/MainActivity.java
@@ -1,6 +1,5 @@
 package io.github.ejchathuranga.customtextview;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,17 +18,18 @@ public class MainActivity extends AppCompatActivity {
 
         final CustomTextView textView = findViewById(R.id.tv);
 
-        textView.setText("CustomTextView is a Customizable text view component that can use on Android");
-
+        textView.setText("CustomTextView is a Customizable text view component that can use on AndroidCustomTextView is a Customizable text view component that can use on AndroidCustomTextView is a Customizable text view component that can use on AndroidCustomTextView is a Customizable text view component that can use on Android");
 
         Button button = findViewById(R.id.button);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                textView.setUnderlineDotWidthAndSpace(55f, 5f);
-
-                textView.setUnderLineColor(Color.CYAN);
+//                textView.setUnderlineDotWidthAndSpace(55f, 5f);
+                textView.setUnderLineColor(0xFFFF00FF);
+//                textView.setUnderlineThickness(20);
+//                textView.setLineSpacing(5,2);
+//                textView.setUnderlinePadding(30);
 
 
             }
